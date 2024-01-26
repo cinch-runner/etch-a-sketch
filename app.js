ad0c40539cb99d9b01ebaf0ed6be2adaf9fb115f
Aaron Sang <caramel.verve.0c@icloud.com>
2024-01-26 10:35:04 -0500

    Added grid size adjustment functionality

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 // DOM Mapping
 let gridContainer = document.querySelector("#grid");
+let gridSizeSlider = document.querySelector("#grid-size-slider");
 
-// let count = prompt("How many? ");
+// load grid on page load
+document.body.onload = gridMaker(16);
 
-document.body.onload = gridMaker;
-
+gridSizeSlider.addEventListener("input", () => {
+  gridMaker(gridSizeSlider.value);
+});
 
 // Creates the grid with 16 rows
-function gridMaker() {
-  for (let x = 0; x < 16; x++) {
+function gridMaker(gridSize) {
+  // clear existing grid
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+
+  // create new grid based on slider value
+  for (let x = 0; x < gridSize; x++) {
     let row = document.createElement("div");
     row.classList.add("row");
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < gridSize; i++) {
       let block = document.createElement("div");
       block.classList.add("block");
       // add event listener here
@@ -27,18 +36,11 @@ function gridMaker() {
 }
 
 // reset button -> resets grid
-
 const resetButton = document.querySelector("#reset");
-
 resetButton.addEventListener("click", () => {
   while (gridContainer.firstChild) {
     gridContainer.removeChild(gridContainer.firstChild);
   }
 
-  gridMaker();
+  gridMaker(16);
 });
-
-// resets grid size to match #grid-size-adjuster
-// on page load, default is 16
-// when slider is moved
-// adjuster grid size to value of slider
